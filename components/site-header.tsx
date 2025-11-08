de0cfa16364549d1ba55d291372fc079bf4a6aac
MeridiaByMNovus <riditjangra09@gmail.com>
2025-11-08 21:38:39 +0530

    chore: aligned header and fixed favicon size

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,28 +1,22 @@
 import React from "react"
 import Link from "next/link"
 
-import { cn } from "@/lib/utils"
-
 import SiteAssets from "./site-assets"
-import { Button, buttonVariants } from "./ui/button"
+import { Button } from "./ui/button"
 
 function SiteHeader() {
   return (
-    <header className={`flex h-20 w-full px-6 lg:px-12`}>
-      <div className={`flex h-full w-1/2 items-center`}>
-        <Link href="/" className="flex items-center">
-          <SiteAssets type="icon" />
-          <h4 className="ml-2 font-heading text-xl">Meridia</h4>
-        </Link>
-      </div>
+    <header className="flex items-center justify-between px-12 sm:mt-6">
+      <Link href="/" className="flex items-center">
+        <SiteAssets type="icon" />
+        <h4 className="ml-2 font-heading text-2xl">Meridia</h4>
+      </Link>
 
-      <div className={`flex h-full w-1/2 items-center justify-end`}>
-        <Link href={"/download"}>
-          <Button variant="fancy" size="lg" className="sm:mt-12">
-            Download
-          </Button>
-        </Link>
-      </div>
+      <Link href={"/download"}>
+        <Button variant="fancy" size="lg">
+          Download
+        </Button>
+      </Link>
     </header>
   )
 }
